066d2755f368e9ad7d54b44e189a04479832762c
ik-semenov <ivan_semenov@tambo.ru>
2017-07-21 19:33:29 +0700

    Add files via upload

--- test2/Controllers/ReportController.cs
+++ test2/Controllers/ReportController.cs
@@ -14,12 +14,12 @@ namespace test2.Controllers
 	[Route("api/Report")]
 	public class ReportController : Controller
 	{
-        DBReportContext db;
+        DBRepository db;
 		IHostingEnvironment _env;
 		public ReportController(IHostingEnvironment env, DBReportContext context)
 		{
 			_env = env;
-            db = context;
+            db = new DBRepository(context);
 		}
 		// GET: api/Report
 		[HttpGet]
@@ -46,40 +46,28 @@ namespace test2.Controllers
             string file_type = "application/json";
             string file_name = "Report";
             return File(doc, file_type, file_name);
-
 		}
 
 		// POST: api/Report
 		[HttpPost]
 		public IActionResult Post([FromBody]Report rp)
 		{
-			string path = _env.ContentRootPath;
-            string temp = rp.GetHash();
-            var rep = db.Reports.Find(temp);
-            DBReport dbr = new DBReport() { Hash = temp, Path = path + @"\Temp\" + Convert.ToString(db.Reports.Count() + 1) + ".pdf", Id = db.Reports.Count() + 1 };
-            if ((rep == null) || (!db.Reports.Any()))
+            if (rp.IsValid())
             {
-                db.Reports.Add(dbr);
-                db.SaveChanges();
-                Task.Run(() => new ReportMaker().CreateReport(rp, path, dbr.Id));
-                return Ok(dbr);
-            }
-            else if ((!System.IO.File.Exists(rep.Path)) && (rep != null))
-            {
-                Task.Run(() => new ReportMaker().CreateReport(rp, path, rep.Id));
-                return Ok(rep);
+                var response = new Solid().Generate(_env, rp, db);
+                return Ok(response);
             }
             else
-                return Ok(rep);
+                return BadRequest();
         }
 
 		// POST: api/Report/5
-		[HttpPost("{id}")]
+		/*[HttpPost("{id}")]
 		public IActionResult Post(int id, [FromBody] Report rp)
 		{
 			if (rp == null)
 				return BadRequest();
 			return Ok(rp.PieChartData["pie1"]);
-		}
+		}*/
 	}
 }
---
+++ test2/Modules/Solid.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using test2.Models;
+
+namespace test2.Modules
+{
+    public class Solid
+    {
+        /*public IHostingEnvironment Env { get; set; }
+        public DBReportContext Context { get; set; }*/
+        /*public Solid()
+        {
+
+        }*/
+        public string Generate(IHostingEnvironment _env, Report rp, DBRepository db)
+        {
+            string path = _env.ContentRootPath;
+            string temp = rp.GetHash();
+            var rep = db.GetReport(temp);
+            DBReport dbr = new DBReport() { Hash = temp, Path = path + @"\Temp\" + Convert.ToString(db.GetCount() + 1) + ".pdf", Id = db.GetCount() + 1 };
+            if ((rep == null) || (db.IsEmpty()))
+            {
+                db.Create(dbr);
+                db.Save();
+                Task.Run(() => new ReportMaker().CreateReport(rp, path, dbr.Id));
+                return "Отчет будет доступен по адресу /api/report/" + dbr.Id.ToString();
+            }
+            else if ((!System.IO.File.Exists(rep.Path)) && (rep != null))
+            {
+                Task.Run(() => new ReportMaker().CreateReport(rp, path, rep.Id));
+                return "Отчет будет доступен по адресу /api/report/" + rep.Id.ToString();
+            }
+            else
+                return "Отчет доступен по адресу /api/report/" + rep.Id.ToString();
+        }
+    }
+}
